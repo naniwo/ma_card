87e304c52b0f66277150a02cd170364614241c12
KoheiAsami2 <mintomidnight2@gmail.com>
2016-10-10 14:09:42 +0900

    カードページへのリンクをdbに取り込み実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CardListPageLink.delete_all
+CardPageLink.delete_all
+
+agent = Mechanize.new
+page = agent.get("https://million-arthurs.gamerch.com/1～10ページ目/#No001")
+
+# card_list_link
+page.links.each do |link|
+  if link.text.match(/ページ目～$/)
+    url = link.uri.to_s
+    url_arr = url.split("/")
+    url_arr.delete_at(url_arr.size - 1)
+    url = url_arr.join("/")
+    CardListPageLink.create!(url: url)
+  end
+end
+
+# card_link
+CardListPageLink.all.each do |list_page_link|
+  p "url: #{list_page_link.url}"
+  agent = Mechanize.new
+  page = agent.get(list_page_link.url)
+  page.search("td").each do |xml|
+    xml.children.each do |child|
+      next unless child.attribute("href") && child.attribute("title")
+      CardPageLink.create(card_name: child.attribute("title").value.split(" - ")[0], url: child.attribute("href").value)
+    end
+  end
+end
